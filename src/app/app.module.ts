34187e2aae2b7fc22d84f551cf879495fbc2e490
hn-ak-x <bohdanvoznyi.x@gmail.com>
2021-05-27 12:50:41 +0300

    homework-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
+import {HttpClientModule} from "@angular/common/http";
+import { PostComponent } from './components/post/post.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
